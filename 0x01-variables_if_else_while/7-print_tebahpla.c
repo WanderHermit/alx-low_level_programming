92a277f12d41126d467973cf20a7e1a00cf87ca1
WanderHermit <mysttrevor16@gmail.com>
2022-12-11 10:18:55 -0800

    alphABET

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,13 @@
 /**
  * main - Entry point
  * Return: Always 0 (success)
- *
  */
 int main(void)
 {
 	char c;
 
-	for (c = 'z'; c <= 'a'; c++)
-		putchar (c);
-	putchar ('\n');
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
 	return (0);
 }
