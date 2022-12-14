d0b54c8f5a05cd8239c6ae3aa8aa1f4ee5996383
WanderHermit <mysttrevor16@gmail.com>
2022-12-13 22:54:56 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main -print putchar
  * Return: 0 (success)
@@ -13,6 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
