caa9a7d435def8f66a5b8ab527f3fec026e07e35
WanderHermit <mysttrevor16@gmail.com>
2023-01-05 05:19:41 -0800

    Your subconcious

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,6 +18,11 @@ int helper(int a, int b)
 
 	return (1);
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
+ * Return: the square root of @n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
